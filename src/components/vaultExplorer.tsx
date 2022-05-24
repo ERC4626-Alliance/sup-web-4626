3fbc30e3a22e5456b384467ded35d91090680769
Sujith Somraaj <somraaj31@gmail.com>
2022-05-24 15:30:31 +0530

    feat: completing fetching from api

--- src/components/vaultExplorer.tsx
+++ src/components/vaultExplorer.tsx
@@ -1,13 +1,49 @@
-import { FunctionComponent } from "react";
-import { vaults } from "../lib/data";
+import axios from "axios";
+import { NextPage } from "next";
+import { FunctionComponent, useEffect, useState } from "react";
+
+interface VaultType {
+  timestamp: String[],
+  chain: String[],
+  contract_address: String[],
+  name: String[],
+  protocol: String[]
+}
+
+const defaultState: VaultType = {
+  timestamp: [],
+  chain: [],
+  name: [],
+  contract_address: [],
+  protocol: []
+}
 
 const VaultExplorer: FunctionComponent = () => {
+  const [vaults, setVaults] =  useState(defaultState)
 
   function classNames(...classes: string[]) {
     return classes.filter(Boolean).join(" ");
   }
 
+  useEffect(() =>  {
+    let config = {
+      headers: {
+        "access-token": "stargate",
+      },
+    };
+    axios
+      .get("https://superform-api.herokuapp.com/explore/", config)
+      .then((res:any) => {
+        console.log(res.data)
+        setVaults(res.data);
+      })
+      .catch((err:any) => {
+        console.log(err);
+      });
+  }, [])
+
   return (
+    vaults === null ? null :
     <section className="px-4 sm:px-6 lg:px-8 pb-16">
       <div className="mt-8 flex flex-col">
         <div className="-my-2 -mx-4 sm:-mx-6 lg:-mx-8">
@@ -24,71 +60,49 @@ const VaultExplorer: FunctionComponent = () => {
                     scope="col"
                     className="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8"
                   >
-                    Vault
+                    Name
                   </th>
                   <th
                     scope="col"
                     className="sticky top-0 z-10 hidden border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:table-cell"
                   >
-                    Protocol
+                    Chain
                   </th>
                   <th
                     scope="col"
                     className="sticky top-0 z-10 hidden border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter lg:table-cell"
                   >
-                    Chain
+                    Protocol
                   </th>
                   <th
                     scope="col"
                     className="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
                   >
-                    APY
-                  </th>
-                  <th
-                    scope="col"
-                    className="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
-                  >
-                    TVL
-                  </th>
-                  <th
-                    scope="col"
-                    className="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
-                  >
-                    Sharpe
+                    Contract Address
                   </th>
                 </tr>
                 </thead>
                 <tbody className="bg-white">
-                {vaults.map((vault, vaultIdx) => (<tr key={`${vault.vault}-${vaultIdx}`}>
-                  <td
-                    className={classNames(vaultIdx !== vaults.length - 1 ? "border-b border-gray-200" : "", "whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8")}
+                {vaults.timestamp.map((vault, vaultIdx) => (<tr key={`${vault}-${vaultIdx}`}>
+                <td
+                    className={classNames(vaultIdx !== vaults.timestamp.length - 1 ? "border-b border-gray-200" : "", "whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8")}
                   >
-                    {vault.vault}
+                    {vaults.name[vaultIdx]}
                   </td>
                   <td
-                    className={classNames(vaultIdx !== vaults.length - 1 ? "border-b border-gray-200" : "", "whitespace-nowrap px-3 py-4 text-sm text-gray-500 hidden sm:table-cell")}
+                    className={classNames(vaultIdx !== vaults.timestamp.length - 1 ? "border-b border-gray-200" : "", "whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8")}
                   >
-                    {vault.protocol}
+                    {vaults.chain[vaultIdx]}
                   </td>
                   <td
-                    className={classNames(vaultIdx !== vaults.length - 1 ? "border-b border-gray-200" : "", "whitespace-nowrap px-3 py-4 text-sm text-gray-500 hidden lg:table-cell")}
+                    className={classNames(vaultIdx !== vaults.timestamp.length - 1 ? "border-b border-gray-200" : "", "whitespace-nowrap px-3 py-4 text-sm text-gray-500 hidden sm:table-cell")}
                   >
-                    {vault.chain}
+                    {vaults.protocol[vaultIdx]}
                   </td>
                   <td
-                    className={classNames(vaultIdx !== vaults.length - 1 ? "border-b border-gray-200" : "", "whitespace-nowrap px-3 py-4 text-sm text-gray-500")}
+                    className={classNames(vaultIdx !== vaults.timestamp.length - 1 ? "border-b border-gray-200" : "", "whitespace-nowrap px-3 py-4 text-sm text-gray-500 hidden lg:table-cell")}
                   >
-                    {vault.apy}
-                  </td>
-                  <td
-                    className={classNames(vaultIdx !== vaults.length - 1 ? "border-b border-gray-200" : "", "whitespace-nowrap px-3 py-4 text-sm text-gray-500")}
-                  >
-                    {vault.tvl}
-                  </td>
-                  <td
-                    className={classNames(vaultIdx !== vaults.length - 1 ? "border-b border-gray-200" : "", "whitespace-nowrap px-3 py-4 text-sm text-gray-500")}
-                  >
-                    {vault.sharpe}
+                    {vaults.contract_address[vaultIdx]}
                   </td>
                 </tr>))}
                 </tbody>
