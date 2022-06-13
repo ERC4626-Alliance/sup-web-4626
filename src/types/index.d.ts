86b2c93b31136b27988394aeab9cc82eed0d20ad
Sergey Beresnev <s.beresnev@matterlabs.dev>
2022-06-13 07:07:56 +0300

    feat: speed and seo optimizations

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,16 +1,2 @@
 import sup from "./sup.d.ts";
-
 export default sup;
-
-declare module "*.jpg" {
-  export default "" as string;
-}
-declare module "*.svg" {
-  export default "" as string;
-}
-declare module "*.webp" {
-  export default "" as string;
-}
-declare module "*.png" {
-  export default "" as string;
-}
