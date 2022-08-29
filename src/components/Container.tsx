cf9ba51d5232b5bc3abd21a01af695251c334b83
Sergey Beresnev <keriat@me.com>
2022-08-29 08:33:46 +0300

    style(responsive): about block fixed

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -3,7 +3,7 @@ import { PropsWithChildren } from "react";
 const Container = (props: PropsWithChildren<{ className?: string }>) => {
   const className = props.className || "";
   return (
-    <section className={`${className} container mx-auto px-5 overflow-x-hidden`} {...props}>
+    <section className={`${className} container mx-auto px-5 overflow-hidden`} {...props}>
       {props.children}
     </section>
   );
