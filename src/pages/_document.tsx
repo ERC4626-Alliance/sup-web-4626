00d7b34b4a92f8f5cfe463b1e638b29fb42d78c2
Sergey Beresnev <keriat@me.com>
2022-08-31 09:19:09 +0300

    fix: fixed deps and json-ld

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,34 +5,9 @@ import Document, {
   Main,
   NextScript,
 } from "next/document";
+import { Question, Answer } from "schema-dts";
+import { jsonLdScriptProps } from "react-schemaorg";
 import { Children } from "react";
-
-const structuredData = `{
-  "@context": "https://schema.org/",
-  "@type": "FAQPage",
-  "mainEntity": [
-    {
-      "@type": "Question",
-      "name": ${process.env.NEXT_SEO_JLD_QUESTION || "What is ERC-4626?"},
-      "acceptedAnswer": {
-        "@type": "Answer",
-        "text": ${
-          process.env.NEXT_SEO_JLD_ANSWER ||
-          "ERC-4626 is a tokenized vault standard. Vaults are smart contracts that take in token deposits and do something with those tokens to provide token rewards to the depositor"
-        }
-      }
-    },
-    {
-      "@type": "Question",
-      "name": "WHY ERC-4626?",
-      "acceptedAnswer": {
-        "@type": "Answer",
-        "text": "Tokenized Vaults have a lack of standardization leading to diverse implementation details. A standard for tokenized Vaults will lower the integration effort" +
-          " for yield - bearing vaults, while creating more consistent and robust implementation patterns."
-      }
-    }
-  ]
-}`;
 
 class CustomDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -44,18 +19,34 @@ class CustomDocument extends Document {
   }
 
   render() {
-    return <Html lang="en">
-      <Head>
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: structuredData }}
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
+    return (
+      <Html lang="en">
+        <Head>
+          <script
+            {...jsonLdScriptProps<Question>({
+              "@context": "https://schema.org",
+              "@type": "Question",
+              name: process.env.NEXT_SEO_JLD_QUESTION
+                ? process.env.NEXT_SEO_JLD_QUESTION
+                : "What is ERC-4626?",
+              answerCount: 1,
+              acceptedAnswer: {
+                "@type": "Answer",
+                text: process.env.NEXT_SEO_JLD_ANSWER
+                  ? process.env.NEXT_SEO_JLD_ANSWER
+                  : "ERC-4626 is a tokenized vault standard. Vaults are smart contracts that take in" +
+                    " token deposits and do something with" +
+                    " those tokens to provide token rewards to the depositor",
+              } as Answer,
+            })}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
 
