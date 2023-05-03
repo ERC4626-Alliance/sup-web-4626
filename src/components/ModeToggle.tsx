03e7b2ac0fa1d35fc1b214b2cfbd10243df53e2e
Sergey Beresnev <keriat@me.com>
2023-05-03 17:08:50 +0300

    style: code styling changed

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -1,14 +1,17 @@
-import React, {useCallback, useEffect} from "react";
-import {BiMoon, BiSun} from "react-icons/bi";
-import {useTheme} from "next-themes";
+import React, { useCallback, useEffect } from "react";
+import { BiMoon, BiSun } from "react-icons/bi";
+import { useTheme } from "next-themes";
 
 export default function ModeToggle() {
-  const {theme, setTheme} = useTheme();
+  const { theme, setTheme } = useTheme();
 
-  const setAndStoreTheme = useCallback((theme: "light" | "dark") => {
-    window.localStorage.colorTheme = theme;
-    setTheme(theme);
-  }, [setTheme]);
+  const setAndStoreTheme = useCallback(
+    (theme: "light" | "dark") => {
+      window.localStorage.colorTheme = theme;
+      setTheme(theme);
+    },
+    [setTheme]
+  );
 
   useEffect(() => {
     if (!window.localStorage.colorTheme) {
@@ -24,13 +27,11 @@ export default function ModeToggle() {
     <button
       type="button"
       aria-label="Toggle dark mode"
-      className="group border-2 border-pink-700 dark:border-pink-500 font-medium text-pink-700 transition-all md:hover:bg-pink-700 dark:md:hover:bg-pink-500 md:hover:text-white md:hover:shadow-md rounded-full dark:bg-transparent px-3 py-2 shadow-lg shadow-zinc-800/5"
+      className="group rounded-full border-2 border-pink-700 px-3 py-2 font-medium text-pink-700 shadow-lg shadow-zinc-800/5 transition-all dark:border-pink-500 dark:bg-transparent md:hover:bg-pink-700 md:hover:text-white md:hover:shadow-md dark:md:hover:bg-pink-500"
       onClick={handleToggle}
     >
-      <BiSun
-        className="h-6 w-6 fill-pink-700 stroke-zinc-500 transition group-hover:fill-zinc-200 group-hover:stroke-zinc-700 dark:hidden dark:fill-teal-50 dark:stroke-teal-500 dark:group-hover:fill-teal-50 dark:group-hover:stroke-teal-600"/>
-      <BiMoon
-        className="hidden h-6 w-6 fill-pink-500 stroke-zinc-500 transition dark:block dark:group-hover:stroke-zinc-400 group-hover:fill-teal-50"/>
+      <BiSun className="h-6 w-6 fill-pink-700 stroke-zinc-500 transition group-hover:fill-zinc-200 group-hover:stroke-zinc-700 dark:hidden dark:fill-teal-50 dark:stroke-teal-500 dark:group-hover:fill-teal-50 dark:group-hover:stroke-teal-600" />
+      <BiMoon className="hidden h-6 w-6 fill-pink-500 stroke-zinc-500 transition group-hover:fill-teal-50 dark:block dark:group-hover:stroke-zinc-400" />
     </button>
   );
 }
